44c79eb4facb44bc5f3e994e84cff0b6891b0e87
James Platt <James.Platt@Neptune.com>
2018-08-30 15:21:16 +0100

    Index column added

--- 7-ORMs-and-Migrations/DataBoard.cs
+++ 7-ORMs-and-Migrations/DataBoard.cs
@@ -10,11 +10,13 @@ namespace _7_ORMs_and_Migrations
     {
         #region "Properties"
 
+        const int indexFieldLength = 5;
         const int nameFieldLength = 20;
         const int jobTitleFieldLength = 20;
         const int salaryFieldLength = 12;
         const int pensionFundContributionsFieldLength = 26;
 
+        const string indexHeader = "Index";
         const string nameHeader = "Name";
         const string jobTitleHeader = "Job Title";
         const string salaryHeader = "Salary";
@@ -33,7 +35,10 @@ namespace _7_ORMs_and_Migrations
         #region "Private Methods"
         private void WriteBody(List<Models.Employees> employees)
         {
-            employees.ForEach(n => Console.WriteLine(GetLine(n)));
+            for(int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(GetLine(employees[i], i.ToString()));
+            }
             Console.WriteLine(GetHorizontalLine());
         }
 
@@ -41,30 +46,31 @@ namespace _7_ORMs_and_Migrations
         {
             Console.WriteLine("Employee database");
             Console.WriteLine(GetHorizontalLine());
-            Console.WriteLine(GetColumns());
+            Console.WriteLine(GetColumnHeaders());
             Console.WriteLine(GetHorizontalLine());
         }
 
         private string GetHorizontalLine()
         {
             // Total length of the table will be equal to the sum of the field lengths as well as a seperator for each field.
-            int numFields = 4;
-            int fieldLength = nameFieldLength + jobTitleFieldLength + salaryFieldLength + pensionFundContributionsFieldLength;
+            int numFields = 5;
+            int fieldLength = indexFieldLength + nameFieldLength + jobTitleFieldLength + salaryFieldLength + pensionFundContributionsFieldLength;
             int totalLength = numFields + fieldLength - 1;
             return "|" + String.Join("", Enumerable.Range(0, totalLength).Select(n => "-")) + "|";
         }
 
-        private string GetColumns()
+        private string GetColumnHeaders()
         {
             var output = new StringBuilder();
-            output.AppendFormat("|{0}|", FormatCell(nameHeader, nameFieldLength));
+            output.AppendFormat("|{0}|", FormatCell(indexHeader, indexFieldLength));
+            output.AppendFormat("{0}|", FormatCell(nameHeader, nameFieldLength));
             output.AppendFormat("{0}|", FormatCell(jobTitleHeader, jobTitleFieldLength));
             output.AppendFormat("{0}|", FormatCell(salaryHeader, salaryFieldLength));
             output.AppendFormat("{0}|", FormatCell(pensionFundContributionsHeader, pensionFundContributionsFieldLength));
             return output.ToString();
         }
 
-        private string GetLine(Models.Employees employee)
+        private string GetLine(Models.Employees employee, string index)
         {
             string name = String.Format("{0} {1}", employee.FirstName, employee.LastName);
             string jobTitle = employee.JobPosition.Title;
@@ -72,7 +78,8 @@ namespace _7_ORMs_and_Migrations
             string pensionFundContributions = String.Format("£{0:n}", (employee.PensionFund?.ContributionAmount).GetValueOrDefault(0));
 
             var output = new StringBuilder();
-            output.AppendFormat("|{0}|", FormatCell(name, nameFieldLength));
+            output.AppendFormat("|{0}|", FormatCell(index, indexFieldLength));
+            output.AppendFormat("{0}|", FormatCell(name, nameFieldLength));
             output.AppendFormat("{0}|", FormatCell(jobTitle, jobTitleFieldLength));
             output.AppendFormat("{0}|", FormatCell(salary, salaryFieldLength));
             output.AppendFormat("{0}|", FormatCell(pensionFundContributions, pensionFundContributionsFieldLength));
