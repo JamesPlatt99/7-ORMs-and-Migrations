f61a495aff855bd99b63c85db374b2b58e42e4d1
James Platt <James.Platt@Neptune.com>
2018-09-04 13:05:31 +0100

    Migration added to add hobby table

---
+++ 7-ORMs-and-Migrations/Migrations/test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(20180409125943)]
+    public class Test : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Hobby")
+              .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+              .WithColumn("Name").AsString().NotNullable();
+
+            Insert.IntoTable("Hobby")
+              .Row(new { Name = "Fishing" })
+              .Row(new { Name = "Drinking" });
+
+            Create.Column("HobbyID").OnTable("Employees").AsInt32().Nullable()
+              .ForeignKey("FK_Employees_Hobby", "Hobby", "Id");
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Employees_Hobby").OnTable("Employees");
+            Delete.Column("HobbyID").FromTable("Employees");
+            Delete.Table("Hobbies");
+        }
+    }
+}
