f8bdbc1598453563883937f41db1d2f916db58b4
James Platt <James.Platt@Neptune.com>
2018-09-05 10:25:14 +0100

    Added update query generator to be used to save any user changes

--- 7-ORMs-and-Migrations/Models/DatabaseObject.cs
+++ 7-ORMs-and-Migrations/Models/DatabaseObject.cs
@@ -5,17 +5,43 @@ using Dapper;
 using System.Data.SqlClient;
 using Dapper.Contrib.Extensions;
 using Dapper.Mapper;
+using System.Text;
 
 namespace _7_ORMs_and_Migrations.Models
 {
-  public class DataBaseObject
-  {
-    protected static readonly string _connectionString =
-      new SqlConnectionStringBuilder
-      {
-        DataSource = "DT283",
-        IntegratedSecurity = true,
-        InitialCatalog = "Employees"
-      }.ConnectionString;
-  }
+    public class DataBaseObject
+    {
+        private Dictionary<string, string> _updates;
+
+        protected static readonly string _connectionString =
+          new SqlConnectionStringBuilder
+          {
+              DataSource = "DT283",
+              IntegratedSecurity = true,
+              InitialCatalog = "Employees"
+          }.ConnectionString;
+        
+        protected void ValueUpdated(string databaseColumn, string value)
+        {
+            _updates = _updates ?? new Dictionary<string, string>();
+            if (_updates.ContainsKey(databaseColumn))
+            {
+                _updates[databaseColumn] = value;
+            }
+            else
+            {
+                _updates.Add(databaseColumn, value);
+            }
+        }
+
+        protected string GetUpdatedValuesQueryString()
+        {
+            var updates = new List<string>();
+            foreach(var update in _updates)
+            {
+                updates.Add(string.Format("{0} = '{1}'", update.Key, update.Value));
+            }
+            return string.Join(", ", updates);
+        }
+    }
 }
